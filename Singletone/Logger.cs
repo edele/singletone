b870fe51fb2ee66e608deab5051bdaaef1a73b73
edele <igrnd0@gmail.com>
2014-02-14 18:48:45 +0600

    init

---
+++ Singletone/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singletone
+{
+    public class Logger
+    {
+        private static Logger instance = null;
+        public string _name;
+        public static Logger GetInstance()
+        {
+            if (instance == null)
+                instance = new Logger();
+            return instance;
+        }
+
+        private Logger()
+        {
+
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello, " + _name);
+        }
+    }
+}
---
+++ Singletone/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singletone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Logger log = new Logger(); Нет доступа к конструктору
+
+            Logger log = Logger.GetInstance();
+            log._name = "Vasya";
+
+            log.SayHello();
+
+            Logger log2 = Logger.GetInstance();
+            log.SayHello();
+            Console.ReadKey(true);
+        }
+    }
+}
