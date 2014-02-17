2b20fefeefa0d433da2a7ba53326c665dd5dc683
edele <igrnd0@gmail.com>
2014-02-18 01:39:06 +0600

    cleaning and commenting

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singletone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Logger log = new Logger(); Нет доступа к конструктору
+
+            Logger log1 = Logger.GetInstance();
+            log1._name = "Vasya";
+            log1.SayHello();
+
+            Logger log2 = Logger.GetInstance();
+            log2.SayHello();
+
+            /* Весь прикол в том, что мы назначили _name для log1, 
+             * а _name оказывается потом и в log2, так как по сути 
+             * в памяти это один и тот же объект.
+             */
+            Console.ReadKey(true);
+        }
+    }
+}
